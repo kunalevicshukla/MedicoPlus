447736e5f9672708bdeeaf3eab5cb6b126af0c18
kunal.shukla <kunal.shukla@theporter.in>
2025-12-31 18:44:15 +0530

    add files for Patient

--- app/src/main/java/org/example/entity/Patient.java
+++ app/src/main/java/org/example/entity/Patient.java
@@ -2,25 +2,50 @@ package org.example.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
 @Entity
 @NoArgsConstructor
+@Getter
+@Setter
+@Table(
+        indexes = {
+                @Index(
+                        name = "dob_idx",
+                        columnList = "dob"
+                )
+        }
+)
 public class Patient {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private LocalDate dob;
     private String email;
-    @Enumerated
+    @Enumerated(EnumType.STRING)
     private Gender gender;
+    @Enumerated(EnumType.STRING)
+    private BloodGroup blood_Group;
 }
 
 enum  Gender {
     MALE,
     FEMALE
+}
+
+enum BloodGroup {
+    A_POSITIVE,
+    A_NEGATIVE,
+    B_POSITIVE,
+    B_NEGATIVE,
+    O_POSITIVE,
+    O_NEGATIVE,
+    AB_POSITIVE,
+    AB_NEGATIVE,
 }
